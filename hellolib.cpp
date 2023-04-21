c31c635f798ffde7c63722e7ea5083438bad0a1a
ilinsdell <89488916+ilinsdell@users.noreply.github.com>
2023-04-21 15:29:15 -0400

    Create hellolib.cpp

---
+++ hellolib.cpp
@@ -0,0 +1,13 @@
+#include "hellolib.h"
+
+//using namespace MyNamespace;
+
+#include <iostream>
+
+namespace MyNamespace
+{
+	void helloliblog()
+	{
+		std::cout << "hello library log line." << std::endl;
+	}
+}
